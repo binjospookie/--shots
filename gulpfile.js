ec60a12073a794ee63fff6fae6858283caf42c53
Victor Pasynok <binjospookie@gmail.com>
2016-10-05 13:35:28 +0300

    add note about buffer to hint

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+const gulp = require('gulp');
+const postcss = require('gulp-postcss');
+
+gulp.task('default', function () {
+    return gulp.src('./styles/*.css').pipe(
+        postcss([
+            require('precss')({ /* options */ })
+        ])
+    ).pipe(
+        gulp.dest('./')
+    );
+});
