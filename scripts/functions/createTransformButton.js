d76bf1f951f06ee353c7b024e8de562430058d69
Victor Pasynok <binjospookie@gmail.com>
2016-10-14 17:41:01 +0300

    Redraw transform button

--- scripts/functions/createTransformButton.js
+++ scripts/functions/createTransformButton.js
@@ -6,11 +6,11 @@ module.exports = function createTransformButton() {
   transform.graphics.beginFill('#37aee2').drawCircle(-10, -10, 10).endFill()
       .beginStroke('#ffffff')
       .setStrokeStyle(2, 'round')
-      .moveTo(-6, -10)
-      .lineTo(-6, -14)
-      .lineTo(-10, -14)
       .moveTo(-14, -10)
-      .lineTo(-14, -6)
+      .lineTo(-14, -14)
+      .lineTo(-10, -14)
+      .moveTo(-6, -10)
+      .lineTo(-6, -6)
       .lineTo(-10, -6)
       .endFill();
   transform.shadow = new createjs.Shadow('rgba(0,0,0,0.19)', 0, 10, 20);
