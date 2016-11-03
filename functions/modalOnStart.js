6724387fc66892e01df2118371c8f07ab45cf855
Victor Pasynok <binjospookie@gmail.com>
2016-11-03 18:54:49 +0300

    Delete modalOnStart.js

--- functions/modalOnStart.js
+++
@@ -1,9 +0,0 @@
-module.exports = function modalOnStart(body, modalWindow) {
-  if (localStorage.getItem('modal') === null) {
-    setTimeout(() => {
-      modalWindow.classList.toggle('open');
-      body.classList.toggle('modal');
-      localStorage.setItem('modal', 'was opened');
-    }, 200);
-  }
-};
