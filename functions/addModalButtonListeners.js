d8aea8c9cc384ab5e2168bcc82cc02837d804ea5
Victor Pasynok <binjospookie@gmail.com>
2016-11-03 18:52:34 +0300

    Delete addModalButtonListeners.js

--- functions/addModalButtonListeners.js
+++
@@ -1,9 +0,0 @@
-module.exports =
-    function
-addModalButtonListeners(closeModalButtons, closeModalButtonClickHandler, body, modalWindow) {
-      Array.prototype.forEach.call(
-          closeModalButtons, (button) => {
-            button.addEventListener('click', closeModalButtonClickHandler.bind(this, body, modalWindow), false);
-          }
-      );
-    };
