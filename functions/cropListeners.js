2e3b22c240444b69843f1f10c2ee9a6c3e23fc86
Victor Pasynok <binjospookie@gmail.com>
2016-11-03 18:53:19 +0300

    Delete cropListeners.js

--- functions/cropListeners.js
+++
@@ -1,6 +0,0 @@
-module.exports = function cropListeners(stageMouseDownHandlerCrop, stageMouseUpHandlerCrop, stageMouseMoveHandlerCrop, stage, body) {
-  body.classList.add('crop');
-  stage.on('stagemousedown', stageMouseDownHandlerCrop);
-  stage.on('stagemouseup', stageMouseUpHandlerCrop);
-  stage.on('stagemousemove', stageMouseMoveHandlerCrop);
-};
