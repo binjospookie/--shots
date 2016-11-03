3ff30b80acba4b3217372bb0bcaf53f7659da9e2
Victor Pasynok <binjospookie@gmail.com>
2016-11-03 18:52:50 +0300

    Delete rectListeners.js

--- functions/rectListeners.js
+++
@@ -1,4 +0,0 @@
-module.exports = function rectListeners(stageMouseDownHandlerRect, stageMouseUpShapes, stage) {
-  stage.on('stagemousedown', stageMouseDownHandlerRect);
-  stage.on('stagemouseup', stageMouseUpShapes);
-};
