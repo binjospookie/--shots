152ead113e5d2fe784959337742b1a6dc4b9c7b0
Victor Pasynok <binjospookie@gmail.com>
2016-11-03 18:54:54 +0300

    Delete setDefaultScene.js

--- functions/setDefaultScene.js
+++
@@ -1,7 +0,0 @@
-module.exports = function setDefaultScene(stage, stageMouseDownHandler, body) {
-  stage.removeAllEventListeners();
-  stage.addEventListener('mousedown', stageMouseDownHandler);
-
-  body.classList.remove('crop');
-  body.classList.remove('move');
-};
