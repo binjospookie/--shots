d9ae4160e14238ca2f643ad143504f672f39462b
Victor Pasynok <binjospookie@gmail.com>
2016-11-03 18:54:13 +0300

    Delete setDefaultFrame.js

--- functions/setDefaultFrame.js
+++
@@ -1,7 +0,0 @@
-module.exports = function setDefaultFrame(frame) {
-  frame.classList.remove('show');
-  frame.style.width = 0;
-  frame.style.height = 0;
-  frame.style.top = 0;
-  frame.style.left = 0;
-};
