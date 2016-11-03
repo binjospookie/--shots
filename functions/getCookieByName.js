b527a190a6356903cd06fed96921a1cdaa994c72
Victor Pasynok <binjospookie@gmail.com>
2016-11-03 18:53:30 +0300

    Delete getCookieByName.js

--- functions/getCookieByName.js
+++
@@ -1,6 +0,0 @@
-module.exports = function getCookie(name) {
-  var matches = document.cookie.match(new RegExp(
-    "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-  ));
-  return matches ? decodeURIComponent(matches[1]) : undefined;
-}
